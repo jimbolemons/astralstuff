8cf6553d3a8f4c9d793e754fccd005388e66e5bb
jacob drummond <drummond.jacob13@gmail.com>
2020-02-04 11:02:30 -0500

    added menu music

---
+++ Assets/Scripts/Audio/AudioMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioMenu : MonoBehaviour
+{
+    public AudioClip startClip;
+    public AudioClip loopClip;
+
+    public AudioSource start;
+    public AudioSource loop;
+    void Start()
+    {
+        loop.loop = true;
+        StartCoroutine(playSound());
+    }
+
+    IEnumerator playSound()
+    {        
+        start.Play();
+        yield return new WaitForSeconds(start.clip.length);       
+        loop.Play();
+    }
+}
+
+
