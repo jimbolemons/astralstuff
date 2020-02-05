28bfdd21a2930e8b5fb16dbd01397ef87a3ffc9f
jacob drummond <drummond.jacob13@gmail.com>
2020-02-05 11:08:53 -0500

    locked the mouse to the screen to make the game easier to play

---
+++ Assets/Scripts/UI/CenterMouse.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CenterMouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Scene m_Scene;
+    string sceneName;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        
+        m_Scene = SceneManager.GetActiveScene();
+        sceneName = m_Scene.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sceneName == "GateCrashing")
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        
+    }
+}
