86ce611835012b639e30985461ad67ff78301cba
jacob drummond <drummond.jacob13@gmail.com>
2020-02-06 07:55:37 -0500

    added place holder music to all playable scenes

--- Assets/Scripts/Audio/AudioMenu.cs
+++ Assets/Scripts/Audio/AudioMenu.cs
@@ -4,8 +4,7 @@ using System.Collections;
 [RequireComponent(typeof(AudioSource))]
 public class AudioMenu : MonoBehaviour
 {
-    public AudioClip startClip;
-    public AudioClip loopClip;
+    
 
     public AudioSource start;
     public AudioSource loop;
