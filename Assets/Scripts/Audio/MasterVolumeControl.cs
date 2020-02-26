3bb6ea5ef4d38668033c1e24c115ad2b35292aeb
jacob drummond <drummond.jacob13@gmail.com>
2020-02-26 09:32:05 -0500

    better volume shit

--- Assets/Scripts/Audio/MasterVolumeControl.cs
+++ Assets/Scripts/Audio/MasterVolumeControl.cs
@@ -4,18 +4,42 @@ using UnityEngine;
 
 public class MasterVolumeControl : MonoBehaviour
 {
-    
+
     [SerializeField]
-    
-   
+    private float num;
+    private void Start()
+    {
+        Invoke("LateStart", 1);
+        
+    }
+
     void Update()
     {
-        AudioListener.volume = FindObjectOfType<AudioManager>().volu;
+        if (AudioListener.volume != num)
+        {
+            AudioListener.volume = num;
+        }
 
     }
     public void OnVolumeChange(float vol)
     {
-        
+        num = vol;
         FindObjectOfType<AudioManager>().VolumeChange(vol);
+    }
+    public void LateStart()
+    {
+        num = FindObjectOfType<AudioManager>().volu;
+        Debug.Log(num);
+    }
+    public void Mute()
+    {
+        if (num > 0)
+        {
+            num = 0;
+        }
+        else
+        {
+            num = 1;
+        }
     }
 }
