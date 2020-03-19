a5c9327b761731c0ae2fb9e650c7db7c099767f9
kaezen <haneryu@gmail.com>
2020-03-19 06:28:44 -0400

    script for changing screen resolution and screentype, still needs testing

---
+++ Assets/ChangeResolution.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChangeResolution : MonoBehaviour
+{
+    //public Dropdown resolutionList;
+    //public Dropdown screenModeList;
+    public TMP_Dropdown resolutionList;
+    public TMP_Dropdown screenModeList;
+    
+
+    public void ApplyChanges()
+    {
+        print(resolutionList.value);
+        print(screenModeList.value);
+
+        int resInt = resolutionList.value;
+        int sModeInt = screenModeList.value;
+
+        FullScreenMode screenMode = FullScreenMode.FullScreenWindow;
+        switch (sModeInt)
+        {
+            case 1:
+                screenMode = FullScreenMode.FullScreenWindow;
+                break;
+            case 2:
+                screenMode = FullScreenMode.ExclusiveFullScreen;
+                break;
+            case 3:
+                screenMode = FullScreenMode.Windowed;
+                break;
+        }
+
+        switch (resInt)
+        {
+            case 0:
+                Screen.SetResolution(1920, 1080, screenMode);
+                break;
+            case 1:
+                Screen.SetResolution(1600, 900, screenMode);
+                break;
+            case 2:
+                Screen.SetResolution(1280, 720, screenMode);
+                break;
+            case 3:
+                Screen.SetResolution(1024, 576, screenMode);
+                break;
+        }
+    }
+}
