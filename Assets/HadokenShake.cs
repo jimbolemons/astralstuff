5e642b46cf7d25665158f79bc1d1eaa82db9f2c8
kaezen <haneryu@gmail.com>
2020-04-04 19:42:49 -0400

    added bullet shake for the hadoken
    started working on the explosion

---
+++ Assets/HadokenShake.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HadokenShake : MonoBehaviour
+{
+    public float max = 3;
+    public float min = -3;
+    public float shakeSpeed = 50;    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float position = Mathf.Lerp(max, min, Mathf.PingPong(Time.time * shakeSpeed, 1));
+        transform.position = new Vector3(transform.position.x + position, transform.position.y, transform.position.z); 
+    }
+}
