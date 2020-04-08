6a885b5efc40590675d0fe92a649a7ea29e92a8e
kaezen <haneryu@gmail.com>
2020-04-08 05:45:16 -0400

    screen shakes when gate breakpoints are hit

--- Assets/Scripts/Health Values/GateHP.cs
+++ Assets/Scripts/Health Values/GateHP.cs
@@ -7,6 +7,10 @@ public class GateHP : ObjectWithHealth
     public bool isDead = false;
     public CameraShake cameraShake;
     public GameObject camera;
+
+    public bool doCameraShake;
+
+    float prevPercent;
     private void Start()
     {
         objectType = objectWithHealthType.destructible;
@@ -15,6 +19,7 @@ public class GateHP : ObjectWithHealth
 
     public void Update()
     {
+        prevPercent = health / maxHealth;
         if (isDead) killGate();
         else
         {
@@ -28,7 +33,7 @@ public class GateHP : ObjectWithHealth
         //Explosions totally go here!
         //print("Barrel go boom");
         isDead = true;
-        
+
     }
 
     private void killGate()
@@ -40,6 +45,19 @@ public class GateHP : ObjectWithHealth
     }
     public override void TriggerOnDamage()
     {
+        //logic for making camera shake (or other thing) when 1/3 breakpoints hit
+        if (doCameraShake)
+        {
+            if (prevPercent >= .66 && health / maxHealth <= .66)
+            {
+                StartCoroutine(cameraShake.Shake(.2f, .4f));
+            }
+            else
+            if (prevPercent >= .33 && health / maxHealth <= .33)
+            {
+                StartCoroutine(cameraShake.Shake(.5f, .6f));
+            }
+        }
         FindObjectOfType<AudioManager>().Play("timeScream");
         //SOUND
     }
