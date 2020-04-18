059f79975548b907c873bafbac6f7385442e254c
kaezen <haneryu@gmail.com>
2020-04-18 08:02:37 -0400

    Gate Health bar has a quick fade in

---
+++ Assets/HealthBarFade.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarFade : MonoBehaviour
+{
+
+    bool fadeIn = false;
+    public float secondsToFadeIn = .5f;
+    float fadeInTimer;
+
+    public CanvasGroup group;
+    void Start()
+    {
+        group.alpha = 0;
+    }
+    private void OnEnable()
+    {
+        fadeIn = true;
+        fadeInTimer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (fadeIn)
+        {
+            fadeInTimer += Time.deltaTime;
+
+            group.alpha = Mathf.Lerp(0, 1, fadeInTimer / secondsToFadeIn);
+            if (fadeInTimer >= secondsToFadeIn)
+            {
+                fadeIn = false;
+            }
+        }
+    }
+}
